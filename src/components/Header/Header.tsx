50a389d6293ac6129ea20571d98144f39de82cd0
Ionito <artemarsupial@gmail.com>
2024-06-01 18:36:47 -0300

    feat: added redirect to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,17 @@
 import logo from '@/assets/logo.svg';
 import clsx from 'clsx';
 import styles from './Header.module.css';
+import { Link } from '@tanstack/react-router';
 
 interface Props extends React.PropsWithChildren {}
 
 export const Header = ({ children }: Props): JSX.Element => {
   return (
     <header className={clsx(styles.header, 'container')}>
-      <div className={styles.logo}>
+      <Link search={{ q: '', page: 1 }} to={'/'} className={styles.logo}>
         <img src={logo} alt="logo" />
         <h1>Latest News</h1>
-      </div>
+      </Link>
 
       {children}
     </header>
